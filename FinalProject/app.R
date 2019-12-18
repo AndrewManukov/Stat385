b439b39ad4ade12aab2a4068773a82d30613a232
Manukov, Andro Sarkis <manukov2@illinois.edu>
2019-12-18 10:54:14 -0600

    Update app.R

--- FinalProject/app.R
+++ FinalProject/app.R
@@ -10,15 +10,15 @@ ui <- fluidPage(
     titlePanel("Chicago Area Food"),
     sidebarLayout(
         sidebarPanel(
-            textInput("addressInput", "Address", ""),
+            textInput("addressInput", "Address", "225 N Columbus Dr, Chicago, IL"),
             sliderInput("milesInput", "Distance From My Address", 0, 35, c(0, 5), post = " Miles"),
-            checkboxGroupInput("facilityType", "Type of Establishment",
+            radioButtons("facilityType", "Type of Establishment",
                                choices = c("Restaurant", "Bar", "Bakery", "Banquet", "Ice Cream", "Pantry", "Paleteria"),
                                selected = "Restaurant"),
-            checkboxGroupInput("riskInput", "Risk Level",
+            radioButtons("riskInput", "Risk Level",
                                choices = c("Risk 3 (Low)", "Risk 2 (Medium)", "Risk 1 (High)"),
                                selected = "Risk 3 (Low)"),
-            checkboxGroupInput("resultInput", "Result", choices = c("Pass", "Fail"), selected = "Pass")
+            radioButtons("resultInput", "Result", choices = c("Pass", "Fail"), selected = "Pass")
             #ADD RADIO BUTTONS
             
         ),
@@ -59,19 +59,16 @@ server <- function(input, output) {
             filter(Distance >= input$milesInput[1],
                    Distance <= input$milesInput[2],
                    Risk == input$riskInput,
-                   `Facility Type` == input$facilityType
+                   `Facility Type` == input$facilityType,
+                   Results == input$resultInput
             )
-        
-        
-        
-        
-        
         
     })
     
     
     
     output$newplot <- renderPlot({
+        
         ggplot(filtered()) +
             geom_point(mapping = aes(x = filtered()$Longitude, y = filtered()$Latitude, color = factor(filtered()$Results))) +
             labs(color = "Conditions") + 
